5f5416334a9690728acdb66129f9fd9b5e678877
wanjugu-wq <wanjuguzzz@gmail.com>
2026-07-10 12:54:54 +0300

    added App.jsx routing

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -1,5 +1,5 @@
-import Navbar from "../components/layout/Navbar";
-import Sidebar from "../components/layout/Sidebar";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 
 const DashboardLayout = ({ children }) => {
   return (
