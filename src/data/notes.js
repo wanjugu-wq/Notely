9495ca8da575993520c3c8758257e2032d2cd4d9
Caleb Finney <calebfinney45@gmail.com>
2026-07-10 12:52:17 +0300

    feat: Add mock data to notes

--- src/data/notes.js
+++ src/data/notes.js
@@ -1,1 +1,23 @@
-export const notes = [];
+export const notes = [
+    {
+    id: 1,
+    title: "Software Project",
+    content: "Finish the React UI before backend integration.",
+    folder: "School",
+    pinned: true,
+    createdAt: "2 hours ago",
+
+    labels: [
+      {
+        id: 1,
+        name: "School",
+        color: "blue",
+      },
+      {
+        id: 2,
+        name: "Important",
+        color: "red",
+      },
+    ],
+  },
+];
