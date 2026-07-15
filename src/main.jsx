900aaade1f22bd07ee0650a1da2ecf18731593b0
Caleb Finney <calebfinney45@gmail.com>
2026-07-15 15:38:57 +0300

    Remove router from there

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,8 @@ import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
