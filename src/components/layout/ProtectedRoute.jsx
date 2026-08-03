9b58c8bd390240d396f8880ece2d8683ee00ef00
adriankamunyu <adrianthegreatone7@gmail.com>
2026-08-03 15:04:36 +0300

    folder layout

--- src/components/layout/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
-
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
